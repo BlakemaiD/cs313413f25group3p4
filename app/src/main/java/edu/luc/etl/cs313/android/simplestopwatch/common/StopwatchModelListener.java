42a7e42c9f48f275a6218f3345b2f68b86a61000
BlakemaiD <150864910+BlakemaiD@users.noreply.github.com>
2025-12-08 11:31:47 -0600

    Update StopwatchModelListener.java

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/common/StopwatchModelListener.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/common/StopwatchModelListener.java
@@ -10,4 +10,8 @@ package edu.luc.etl.cs313.android.simplestopwatch.common;
 public interface StopwatchModelListener {
     void onTimeUpdate(int timeValue);
     void onStateUpdate(int stateId);
+
+    void onBeep(); //the sound
+    void onAlarmStart(); //start beeping ---->See final commit on StopwatchAdapter for details on the usage
+    void onAlarmStop(); //stop beeping
 }
