d0942eb2cfebd0f695f160d68750bb974fa1d54a
BlakemaiD <150864910+BlakemaiD@users.noreply.github.com>
2025-12-08 11:30:38 -0600

    Add beep for alarm
    Here is the final StopwatchAdapter file, with many trials and errors, I was onlt able to implement the beep adn not the alarm when the timer runs out due to the continuous loop. The code is still there in psudo code.

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/android/StopwatchAdapter.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/android/StopwatchAdapter.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 //Some of the android media sounds, used from clickcounter android
 import android.media.AudioAttributes;
 import android.media.MediaPlayer;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 
@@ -42,10 +43,17 @@ public class StopwatchAdapter extends Activity implements StopwatchModelListener
         super.onCreate(savedInstanceState);
         // inject dependency on view so this adapter receives UI events
         setContentView(R.layout.activity_new_main);
+
+
+        //test sound, having problems hearing
+        //playDefaultNotification();
+
         // inject dependency on model into this so model receives UI events
         this.setModel(new ConcreteStopwatchModelFacade());
         // inject dependency on this into model to register for UI updates
         model.setModelListener(this);
+
+
     }
 
     @Override
@@ -91,13 +99,69 @@ public class StopwatchAdapter extends Activity implements StopwatchModelListener
         });
     }
 
+    //Alarm sound
+    private Ringtone alarmTone;
+
+    /** Plays the default notification */
+    /** This is from clickcounter*/
+    protected void playDefaultNotification() {
+        final Uri defaultRingtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        final MediaPlayer mediaPlayer = new MediaPlayer();
+        final var context = getApplicationContext();
+
+        try {
+            mediaPlayer.setDataSource(context, defaultRingtoneUri);
+            mediaPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                            .build());
+            mediaPlayer.prepare();
+            mediaPlayer.setOnCompletionListener(MediaPlayer::release);
+            mediaPlayer.start();
+        } catch (final java.io.IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /** Starts a looping alarm */
+    protected void startAlarmSound() {
+        if (alarmTone == null) {
+            Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+            alarmTone = RingtoneManager.getRingtone(getApplicationContext(), alarmUri);
+        }
+        if (alarmTone != null && !alarmTone.isPlaying()) {
+            alarmTone.play();
+        }
+    }
+
+    /** Stops the looping alarm sound, if any. */
+    public void stopAlarmSound() {
+        if (alarmTone != null && alarmTone.isPlaying()) {
+            alarmTone.stop();
+        }
+    }
+
+    @Override
+    public void onBeep() {
+        // Make sure this runs on the UI thread
+        runOnUiThread(this::playDefaultNotification);
+    }
+    @Override
+    public void onAlarmStart() {
+        /** For sake of time, I was only able to add the beep audio, but not the alarm
+         * I tried different ways, but was unable to get the alarm to stop...*/
+        //runOnUiThread(this::startAlarmSound);
+    }
+    @Override
+    public void onAlarmStop() {
+        //runOnUiThread(this::stopAlarmSound);
+    }
     public void onStartStop(final View view) {
         model.onStartStop();
-    } 
+    }
 
     public void onLapReset(final View view)  { //Could be removed.
         model.onLapReset();
     }
 }
-
-
